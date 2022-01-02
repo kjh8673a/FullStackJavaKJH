3e122811a2d36c7f8268df97db76d7eb005e6d5f
kjh8673a <kjh8673a@naver.com>
2022-01-02 19:41:47 +0900

    Day03

--- Homework1/Day03/src/Animal.java
+++ Homework1/Day03/src/Animal.java
@@ -1,3 +1,5 @@
+// 문제) 객체 지향 방식으로 Animal의 class를 만들고, Animal의 속성을 2가지 지정하고, 메서드도 2가지를 만들고나서, OOP.java에서 객체화 한 다음 하나의 속성을 실행하고 나머지 메서드도 실행하는 코드를 작성하세요.
+
 public class Animal {
     public static void main(String[] args) {
         Animaloop dog = new Animaloop();
--- Homework1/Day03/src/SumAverage.java
+++ Homework1/Day03/src/SumAverage.java
@@ -1,3 +1,5 @@
+// 문제) 배열에 100, 88, 100, 100, 90 을 입력하고 총합과 평균을 출력하시오.
+
 public class SumAverage {
     public static void main(String[] args) {
         int[] score = new int[] { 100, 88, 100, 100, 90 };
